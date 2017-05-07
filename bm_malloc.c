7d4b32b2413447531f8cc0488c9a9f99335e7237
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2017-05-07 09:57:13 +0900

    Fix malloc benchmark program

--- bm_malloc.c
+++ bm_malloc.c
@@ -1,95 +1,80 @@
+/*
+ * Fixed size malloc on fragmented heap
+ *
+ * output is:
+ *  log2(bytes)
+ *  K&R malloc ave ticks
+ *  K&R malloc worst ticks
+ *  K&R free ave ticks
+ *  K&R free worst ticks
+ *  Doug Lea's malloc ave ticks
+ *  Doug Lea's malloc worst ticks
+ *  Doug Lea's free ave ticks
+ *  Doug Lea's free worst ticks
+ */
+
 #include "types.h"
 #include "user.h"
 
-#define stdout 1
-#define stderr 2
+#define TEST_NUM (1 << 5)
+#define ALLOC_NUM_LOG2 22
 
-#define TEST_NUM (1 << 10)
-#define ALLOC_NUM (1 << 18)
+#define ALLOC_SIZE 16
 
-size_t sizes[ALLOC_NUM];
-void* ptrs[ALLOC_NUM];
-
-size_t randstate;
-size_t rand()
-{
-    randstate = randstate * 1664525 + 1013904223;
-    return randstate;
-}
+void* ptrs[1 << ALLOC_NUM_LOG2];
 
 int main(void)
 {
-    int i, j;
-    size_t s;
-    int start_ticks, end_ticks, sum_ticks, ave_ticks, worst_ticks;
+    int a, t, i;
+    int start_ticks, end_ticks, ticks;
+    int malloc_ave_ticks, malloc_worst_ticks;
+    int free_ave_ticks, free_worst_ticks;
 
-    printf(stdout, "# small size malloc\n");
-    for (s = 16; s <= 512; s += 8) {
-        printf(stdout, "%d ", s);
+    printf(1,
+        "# fixed-size malloc on fragmented heap (TEST_NUM = %d)\n", TEST_NUM);
 
-        ave_ticks = 0;
-        worst_ticks = 0;
-        for (j = 0; j < TEST_NUM; j++) {  // malloc
-            start_ticks = uptime();
-            for (i = 0; i < ALLOC_NUM; i++)
-                ptrs[i] = malloc(s);
-            end_ticks = uptime();
-            sum_ticks = end_ticks - start_ticks;
-            ave_ticks += sum_ticks;
-            worst_ticks = worst_ticks < sum_ticks ? sum_ticks : worst_ticks;
+#define max(x, y) (x > y ? x : y)
 
-            for (i = 0; i < ALLOC_NUM; i++)
-                free(ptrs[i]);
-        }
+#define TICKS_END_UPDATE(AVE, WORST) \
+    end_ticks = uptime();            \
+    ticks = end_ticks - start_ticks; \
+    AVE += ticks;                    \
+    WORST = max(WORST, ticks)
 
-        printf(stdout, "%d %d ", ave_ticks / TEST_NUM, worst_ticks);
+#define RUN(ALLOC_NUM, ALLOC, FREE)                             \
+    malloc_ave_ticks = malloc_worst_ticks = 0;                  \
+    free_ave_ticks = free_worst_ticks = 0;                      \
+                                                                \
+    for (t = 0; t < TEST_NUM; t++) {                            \
+        ptrs[0] = ALLOC(2 * ALLOC_SIZE);                        \
+        for (i = 1; i < ALLOC_NUM; i++)                         \
+            ptrs[i] = ALLOC(ALLOC_SIZE);                        \
+        for (i = 0; i < ALLOC_NUM; i += 2)                      \
+            FREE(ptrs[i]);                                      \
+                                                                \
+        start_ticks = uptime();                                 \
+        ptrs[0] = ALLOC(2 * ALLOC_SIZE);                        \
+        TICKS_END_UPDATE(malloc_ave_ticks, malloc_worst_ticks); \
+                                                                \
+        FREE(ptrs[ALLOC_NUM - 1]);                              \
+        start_ticks = uptime();                                 \
+        FREE(ptrs[0]);                                          \
+        TICKS_END_UPDATE(free_ave_ticks, free_worst_ticks);     \
+                                                                \
+        for (i = 1; i < ALLOC_NUM - 1; i += 2)                  \
+            FREE(ptrs[i]);                                      \
+    }                                                           \
+                                                                \
+    printf(1, "%d %d %d %d ",                                   \
+        malloc_ave_ticks, malloc_worst_ticks,                   \
+        free_ave_ticks, free_worst_ticks)
 
-        ave_ticks = 0;
-        worst_ticks = 0;
-        for (j = 0; j < TEST_NUM; j++) {  // dlmalloc
-            start_ticks = uptime();
-            for (i = 0; i < ALLOC_NUM; i++)
-                ptrs[i] = dlmalloc(s);
-            end_ticks = uptime();
-            sum_ticks = end_ticks - start_ticks;
-            ave_ticks += sum_ticks;
-            worst_ticks = worst_ticks < sum_ticks ? sum_ticks : worst_ticks;
-
-            for (i = 0; i < ALLOC_NUM; i++)
-                dlfree(ptrs[i]);
-        }
-
-        printf(stdout, "%d %d\n", ave_ticks / TEST_NUM, worst_ticks);
+    for (a = 8; a <= ALLOC_NUM_LOG2; a++) {
+        printf(1, "%d ", a);
+        RUN((1 << a), malloc, free);
+        RUN((1 << a), dlmalloc, dlfree);
+        printf(1, "\n");
     }
-
-#if 0
-    printf(stdout, "# random small size\n");
-    randstate = uptime();
-    for (i = 0; i < ALLOC_NUM; i++)
-        sizes[i] = (rand() % 31 + 2 /* 2 - 32 */) * 8; /* 16 - 256 */
-
-    for (sum_ticks = 0, j = 0; j < TEST_NUM; j++) {  // malloc
-        start_ticks = uptime();
-        for (i = 0; i < ALLOC_NUM; i++)
-            ptrs[i] = malloc(sizes[i]);
-        for (i = ALLOC_NUM - 1; i >= 0; i--)
-            free(ptrs[i]);
-        end_ticks = uptime();
-        sum_ticks += end_ticks - start_ticks;
-    }
-    printf(stdout, "%d ", sum_ticks);
-
-    for (sum_ticks = 0, j = 0; j < TEST_NUM; j++) {  // dlmalloc
-        start_ticks = uptime();
-        for (i = 0; i < ALLOC_NUM; i++)
-            ptrs[i] = dlmalloc(sizes[i]);
-        for (i = ALLOC_NUM - 1; i >= 0; i--)
-            dlfree(ptrs[i]);
-        end_ticks = uptime();
-        sum_ticks += end_ticks - start_ticks;
-    }
-    printf(stdout, "%d\n", sum_ticks);
-#endif
 
     exit();
 }
